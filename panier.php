389d5a439d66ae120318880b914421b371a7908e
EwanLg <ewan.legall22@gmail.com>
2025-01-14 14:40:35 +0100

    Update panier.php

--- panier.php
+++ panier.php
@@ -20,8 +20,44 @@
 
  <div class="col-md-9">
  <?php
- echo $_SESSION['panier']
- ?>
+
+if (isset($_GET['action']) && $_GET['action'] == 'supprimer' && isset($_GET['nolivre'])) {
+    $nolivre = $_GET['nolivre'];
+    if (isset($_SESSION['panier'][$nolivre])) {
+        unset($_SESSION['panier'][$nolivre]);
+        $message = "Le livre a été supprimé du panier."; 
+    } else {
+        $message = "Le livre n'existe pas dans le panier."; 
+    }
+}
+
+if (isset($_SESSION['panier']) && !empty($_SESSION['panier'])) {
+    foreach ($_SESSION['panier'] as $key => $book) {
+        echo "<div class='card mb-3' id='book-{$key}' style='max-width: 540px;'>
+                <div class='row g-0'>
+                    <div class='col-md-4'>
+                        <!-- Ajuster la taille de l'image -->
+                        <img src='covers/{$book['photo']}' class='img-fluid rounded-start' alt='{$book['titre']}' style='max-height: 150px; object-fit: cover;'>
+                    </div>
+                    <div class='col-md-8'>
+                        <div class='card-body'>
+                            <h5 class='card-title'>{$book['titre']}</h5>
+                            <p class='card-text'>Auteur: {$book['auteur']}</p>
+                            <p><a href='?nolivre={$key}&action=supprimer' class='btn btn-danger'>Supprimer</a></p>
+                        </div>
+                    </div>
+                </div>
+              </div>";
+    }
+} else {
+    echo "<p>Votre panier est vide.</p>";
+}
+
+if (isset($message)) {
+    echo "<p class='alert alert-info'>{$message}</p>";
+}
+?>
+
 </div>
 
  <div class="col-md-3">
